368b5ffa2fa6981716fdc490da719dbd6dc51a31
Денис Сапачев <denissapacev@macbook-pro-denis.local>
2018-08-03 22:06:41 +0300

    123

--- ATX_01/ViewController.swift
+++ ATX_01/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         //some comment
         //todo
+        //123
         // Do any additional setup after loading the view, typically from a nib.
     }
 
